4b8796de15b270f7f496688d342d1d9c535c5c99
kirzan101 <ckimescamilla@gmail.com>
2025-06-04 00:22:32 +0800

    adjsut snack bar icon

--- resources/js/Components/Utilities/SnackBar.vue
+++ resources/js/Components/Utilities/SnackBar.vue
@@ -1,22 +1,47 @@
 <template>
-    <v-snackbar v-model="snackBar" :color="props.color" :timeout="timeout">
+    <v-snackbar
+        v-model="snackBar"
+        :color="props.color"
+        :timeout="props.timeout"
+    >
         {{ props.message }}
 
         <template v-slot:actions="{ attrs }">
-            <v-btn
-                color="white"
-                variant="text"
-                v-bind="attrs"
-                @click="toggleNotification(false)"
-                icon="mdi-close-circle"
-            >
-            </v-btn>
+            <v-hover v-bind="attrs">
+                <template v-slot:default="{ isHovering, props }">
+                    <span v-bind="props">
+                        <v-btn
+                            v-if="isHovering"
+                            color="white"
+                            variant="text"
+                            @click="toggleNotification(false)"
+                            icon="mdi-close-circle"
+                        >
+                        </v-btn>
+
+                        <v-progress-circular
+                            v-else
+                            color="white"
+                            :size="20"
+                            :width="2"
+                            class="mr-3"
+                            indeterminate
+                        >
+                            <template v-slot:default>
+                                <span style="font-size: 10px">{{
+                                    countdown
+                                }}</span>
+                            </template>
+                        </v-progress-circular></span
+                    >
+                </template>
+            </v-hover>
         </template>
     </v-snackbar>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch, onUnmounted } from "vue";
 
 const props = defineProps({
     message: {
@@ -34,6 +59,34 @@ const props = defineProps({
 });
 
 const snackBar = ref(false);
+const countdown = ref(Math.ceil(props.timeout / 1000));
+let timer = null;
+
+const startCountdown = () => {
+    countdown.value = Math.ceil(props.timeout / 1000);
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if (countdown.value > 0) {
+            countdown.value--;
+        }
+        if (countdown.value <= 0) {
+            clearInterval(timer);
+            toggleNotification(false);
+        }
+    }, 1000);
+};
+
+watch(snackBar, (val) => {
+    if (val) {
+        startCountdown();
+    } else {
+        clearInterval(timer);
+    }
+});
+
+onUnmounted(() => {
+    clearInterval(timer);
+});
 
 const toggleNotification = (value = true) => {
     snackBar.value = value;
